34f84331e0416418e11524c41b75c274a7382f50
Luke Bangs <bangsluke@gmail.com>
2022-09-06 22:18:11 +0100

    Initial images added and summary page begun

--- src/pages/summary.tsx
+++ src/pages/summary.tsx
@@ -2,6 +2,9 @@
 import { Grid } from '@mui/material';
 import { gridSpacing } from 'store/constant';
 
+import Image from 'next/image';
+import BigLynn2023 from '../../public/assets/images/2022-Lineup.jpg';
+
 // Project imports
 import Meta from '../components/base/Meta';
 import MainCard from 'ui-component/cards/MainCard';
@@ -15,6 +18,18 @@ import I18nS from '../services/I18nS';
 const Summary = () => (
   <>
     <Meta title={I18nS.t('INPUTS_title')} description={I18nS.t('INPUTS_description')} />
+    <MainCard title="Header">
+      <Grid container spacing={gridSpacing}>
+        <Grid item xs={12}>
+          <Grid container alignItems="center" justifyContent="space-between" spacing={gridSpacing}>
+            <Grid item lg={12} md={12} sm={12} xs={12}>
+              Hello
+              <Image src={BigLynn2023} alt="Big Lynn 2023" layout="intrinsic" width={'400'} height={'270'} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </MainCard>
     <MainCard title="Summary">
       <Grid container spacing={gridSpacing}>
         <Grid item xs={12}>
