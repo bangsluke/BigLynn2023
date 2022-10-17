3d7aad80f1c2d1999bf411c907e9cae3a0770837
Luke Bangs <bangsluke@gmail.com>
2022-10-17 18:39:03 +0100

    Added AOS library

--- src/components/features/ItinerarySection.tsx
+++ src/components/features/ItinerarySection.tsx
@@ -1,9 +1,17 @@
 import { Container, Grid, Typography } from '@mui/material';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { useEffect } from 'react';
 import ThemingS from 'services/ThemingS';
 
 export default function ItinerarySection() {
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+    AOS.refresh();
+  }, []);
+
   return (
-    <Container>
+    <Container data-aos="fade-left" data-aos-duration="3000">
       <Grid container spacing={ThemingS.themeConfig.gridSpacing}>
         {/* Hold the section header text */}
         <Grid item xs={12} lg={5} md={10}>
@@ -32,13 +40,27 @@ export default function ItinerarySection() {
           <Typography variant="body1" component="div">
             Itinerary text - 2 nights&#39; dinner, bed & breakfast at the Stoke by Nayland Hotel, Golf & Spa and 3 rounds of golf (2 on
             Gainsborough Course, 1 on Constable Course)
-            <ul>• At 12:40 18 holes on Gainsborough Course (Sat 15 Jul 2023)</ul>
-            <ul>• From 15:00 Check-in at Stoke by Nayland Hotel, Golf & Spa for 2 nights (Golf Deluxe Twin room) (Sat 15 Jul 2023) </ul>
-            <ul>• At 20:00 Dinner at The Restaurant, standard menu (Sat 15 Jul 2023) </ul>
-            <ul>• At 12:40 18 holes on Constable Course (Sun 16 Jul 2023)</ul>
-            <ul>• At 20:00 Dinner at The Restaurant, standard menu (Sun 16 Jul 2023) </ul>
-            <ul>• At 10:40 18 holes on Gainsborough Course (Mon 17 Jul 2023) </ul>
-            <ul>• By 11:00 Check-out of Stoke by Nayland Hotel, Golf & Spa (Mon 17 Jul 2023)</ul>
+            <ul data-aos="fade-in" data-aos-duration="3000" data-aos-delay="50">
+              • At 12:40 18 holes on Gainsborough Course (Sat 15 Jul 2023)
+            </ul>
+            <ul data-aos="fade-in" data-aos-duration="3000" data-aos-delay="100">
+              • From 15:00 Check-in at Stoke by Nayland Hotel, Golf & Spa for 2 nights (Golf Deluxe Twin room) (Sat 15 Jul 2023){' '}
+            </ul>
+            <ul data-aos="fade-in" data-aos-duration="3000" data-aos-delay="150">
+              • At 20:00 Dinner at The Restaurant, standard menu (Sat 15 Jul 2023){' '}
+            </ul>
+            <ul data-aos="fade-in" data-aos-duration="3000" data-aos-delay="200">
+              • At 12:40 18 holes on Constable Course (Sun 16 Jul 2023)
+            </ul>
+            <ul data-aos="fade-in" data-aos-duration="3000" data-aos-delay="250">
+              • At 20:00 Dinner at The Restaurant, standard menu (Sun 16 Jul 2023){' '}
+            </ul>
+            <ul data-aos="fade-in" data-aos-duration="3000" data-aos-delay="300">
+              • At 10:40 18 holes on Gainsborough Course (Mon 17 Jul 2023){' '}
+            </ul>
+            <ul data-aos="fade-in" data-aos-duration="3000" data-aos-delay="350">
+              • By 11:00 Check-out of Stoke by Nayland Hotel, Golf & Spa (Mon 17 Jul 2023)
+            </ul>
           </Typography>
         </Grid>
       </Grid>
