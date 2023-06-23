c58730d3c6967410ff1ee163c42446e039572e77
Luke Bangs <bangsluke@gmail.com>
2023-06-23 22:18:17 +0100

    Extracted each rules section to allow customisation of layout

--- src/components/features/RulesSection.tsx
+++ src/components/features/RulesSection.tsx
@@ -34,6 +34,117 @@ export default function RulesSection() {
 		},
 	};
 
+	// Extract each of the sections into their own components to allow customisation of layouts based upon screen size
+	const PrincipalCompetitionPointsSection = () => {
+		return (
+			<Grid container direction='row' spacing={2}>
+				<Grid item xs={12} lg={6}>
+					<Typography variant='h4' component='div' id='competitionPoints' className='offset' sx={Styles.sectionHeader}>
+						Principal competition points
+					</Typography>
+					<Typography variant='body1' component='div' sx={Styles.sectionText}>
+						As the trophy returned to the Bangs clan last year, it makes complete sense to keep points arrangements and the like the same
+						for 2023, so no tinkering this time around.
+					</Typography>
+					<Typography variant='body1' component='div' sx={Styles.sectionText}>
+						Points will be scored each day based upon the following table, depending upon your Stableford points score, which takes into
+						account the relative handicaps:
+					</Typography>
+				</Grid>
+				<Grid item xs={12} lg={6} sx={{ pl: 2, pr: 2 }}>
+					<ScoresTable />
+				</Grid>
+			</Grid>
+		);
+	};
+
+	const BonusPointsSection = () => {
+		return (
+			<>
+				<Typography variant='h4' component='div' id='bonusPoints' className='offset' sx={Styles.sectionHeader}>
+					Bonus points
+				</Typography>
+				<Typography variant='body1' component='div' sx={Styles.sectionText}>
+					Three bonus points will be available on each of two holes on all of Saturday, Sunday and Monday, as drawn randomly – these are
+					called “bullet holes” and everyone who equals the best net scores on those holes will receive the three points.
+				</Typography>
+			</>
+		);
+	};
+
+	const HandicapAdjustmentsSection = () => {
+		return (
+			<Grid container direction='row' spacing={2} sx={{ mb: 0, width: "100%" }}>
+				<Grid item xs={12} lg={6}>
+					<Typography variant='h4' component='div' id='handicapAdjustments' className='offset' sx={Styles.sectionHeader}>
+						Handicap Adjustments
+					</Typography>
+					<Typography variant='body1' component='div' sx={Styles.sectionText}>
+						In a change of practice, it appears that we decided to amend the starting handicaps for 2023 based upon adjusting the final day
+						handicaps in 2022. Therefore the starting handicaps for 2023 are as follows:
+					</Typography>
+
+					<Grid item xs={12} lg={12} sx={{ pl: 2, pr: 2 }}>
+						<Handicaps2023Table />
+					</Grid>
+
+					<Typography variant='body1' component='div' sx={Styles.sectionText}>
+						Handicap changes for day 2 and day 3 will as always be based upon the performance from the previous day. With 11 competitors,
+						the following matrix is to be applied based upon your finishing score.
+					</Typography>
+					<Typography variant='body1' component='div' sx={Styles.sectionText}>
+						Where there are positional ties, all competitors will be adjusted as if they had finished in the tied position – i.e. if two
+						players are tied for 2nd on a day, they will both be adjusted as if they finished 2nd by themselves.
+					</Typography>
+				</Grid>
+				<Grid item xs={12} lg={6} sx={{ pl: 2, pr: 2 }}>
+					<HandicapAdjustmentTable />
+				</Grid>
+			</Grid>
+		);
+	};
+
+	const SpecialSHITRulesSection = () => {
+		return (
+			<>
+				<Typography variant='h4' component='div' id='SHITRules' className='offset' sx={Styles.sectionHeader}>
+					Special SHIT Rules (“Angel Poo”)
+				</Typography>
+				<Typography variant='body1' component='div' sx={Styles.sectionText}>
+					On all golfing days, the following special rules will apply:
+					<ul style={Styles.unorderedList}>- Mulligan or Reverse Mulligan (1 per round per player, cannot be carried over)</ul>
+					<ul style={Styles.unorderedList}>- Fairway Robbery (1 per round per player) [NB. There are no reverse Fairway Robbery]</ul>
+					<ul style={Styles.unorderedList}>- Big Gimmee (1 per round per player)</ul>
+					<ul style={Styles.unorderedList}>
+						- Joker (1 per round per player) to be called before playing tee shot on any hole for double points on that hole
+					</ul>
+					<ul style={Styles.unorderedList}>- String Theory (Sunday only) - 7 to 1 foot lengths of string drawn at random</ul>
+					<ul style={Styles.unorderedList}>- Big Girl&#39;s Blouse (1 per round per player)</ul>
+				</Typography>
+				<Typography variant='body1' component='div' sx={Styles.sectionText}>
+					Dave Rose (Official Golf Nerd) to hold casting vote any real “Rules of Golf” issues.
+				</Typography>
+			</>
+		);
+	};
+
+	const PrizesSection = () => {
+		return (
+			<>
+				<Typography variant='h4' component='div' id='prizes' className='offset' sx={Styles.sectionHeader}>
+					Prizes may be available for the following categories:
+				</Typography>
+				<Typography variant='body1' component='div' sx={Styles.sectionText}>
+					<ul style={Styles.unorderedList}>- Overall competition winner (may include trophy)</ul>
+					<ul style={Styles.unorderedList}>- Longest drive (Sunday)</ul>
+					<ul style={Styles.unorderedList}>- Nearest the pin any hole (Sunday)</ul>
+					<ul style={Styles.unorderedList}>- Longest drive (Monday)</ul>
+					<ul style={Styles.unorderedList}>- Nearest the pin any hole (Monday)</ul>
+				</Typography>
+			</>
+		);
+	};
+
 	return (
 		<Container>
 			<Grid container spacing={ThemingS.themeConfig.gridSpacing} sx={{ minHeight: RuleSectionHeight }}>
@@ -47,7 +158,7 @@ export default function RulesSection() {
 				{/* Hold the contents section and body section alongside one another */}
 				<Grid item xs={12} sx={{ mb: 4, backgroundColor: "null" }}>
 					<Grid container direction='row' spacing={ThemingS.themeConfig.gridSpacing}>
-						{/* Hold the contents information */}
+						{/* Hold the contents information on the left side */}
 						<Grid item xs={12} sm={4} md={3} lg={3} xl={3} sx={{ mb: 2, backgroundColor: "null" }}>
 							<Typography variant='h4' component='div'>
 								Contents
@@ -83,106 +194,29 @@ export default function RulesSection() {
 								}}
 								ref={appScrollComponent}>
 								<Grid container spacing={2} sx={{ mb: 0 }}>
-									{/* Points Scoring */}
+									{/* Points Scoring - Principal competition points */}
 									<Grid item xs={12} sx={{ height: "max-content", mb: 2, mr: 2 }}>
-										<Grid container direction='row' spacing={2}>
-											<Grid item xs={12} lg={6}>
-												<Typography variant='h4' component='div' id='competitionPoints' className='offset' sx={Styles.sectionHeader}>
-													Principal competition points
-												</Typography>
-												<Typography variant='body1' component='div' sx={Styles.sectionText}>
-													As the trophy returned to the Bangs clan last year, it makes complete sense to keep points arrangements and the
-													like the same for 2023, so no tinkering this time around.
-												</Typography>
-												<Typography variant='body1' component='div' sx={Styles.sectionText}>
-													Points will be scored each day based upon the following table, depending upon your Stableford points score, which
-													takes into account the relative handicaps:
-												</Typography>
-											</Grid>
-											<Grid item xs={12} lg={6} sx={{ pl: 2, pr: 2 }}>
-												<ScoresTable />
-											</Grid>
-										</Grid>
+										<PrincipalCompetitionPointsSection />
 									</Grid>
 
 									{/* Bonus Points */}
 									<Grid item xs={12} sx={{ height: "max-content", mb: 2 }}>
-										<Typography variant='h4' component='div' id='bonusPoints' className='offset' sx={Styles.sectionHeader}>
-											Bonus points
-										</Typography>
-										<Typography variant='body1' component='div' sx={Styles.sectionText}>
-											Three bonus points will be available on each of two holes on all of Saturday, Sunday and Monday, as drawn randomly –
-											these are called “bullet holes” and everyone who equals the best net scores on those holes will receive the three
-											points.
-										</Typography>
+										<BonusPointsSection />
 									</Grid>
 
 									{/* Handicap Adjustments */}
 									<Grid item xs={12} sx={{ height: "max-content", mb: 2 }}>
-										<Grid container direction='row' spacing={2} sx={{ mb: 0, width: "100%" }}>
-											<Grid item xs={12} lg={6}>
-												<Typography variant='h4' component='div' id='handicapAdjustments' className='offset' sx={Styles.sectionHeader}>
-													Handicap Adjustments
-												</Typography>
-												<Typography variant='body1' component='div' sx={Styles.sectionText}>
-													In a change of practice, it appears that we decided to amend the starting handicaps for 2023 based upon adjusting
-													the final day handicaps in 2022. Therefore the starting handicaps for 2023 are as follows:
-												</Typography>
-
-												<Grid item xs={12} lg={6} sx={{ pl: 2, pr: 2 }}>
-													<Handicaps2023Table />
-												</Grid>
-
-												<Typography variant='body1' component='div' sx={Styles.sectionText}>
-													Handicap changes for day 2 and day 3 will as always be based upon the performance from the previous day. With 11
-													competitors, the following matrix is to be applied based upon your finishing score.
-												</Typography>
-												<Typography variant='body1' component='div' sx={Styles.sectionText}>
-													Where there are positional ties, all competitors will be adjusted as if they had finished in the tied position –
-													i.e. if two players are tied for 2nd on a day, they will both be adjusted as if they finished 2nd by themselves.
-												</Typography>
-											</Grid>
-											<Grid item xs={12} lg={6} sx={{ pl: 2, pr: 2 }}>
-												<HandicapAdjustmentTable />
-											</Grid>
-										</Grid>
+										<HandicapAdjustmentsSection />
 									</Grid>
 
-									{/* SHIT Rules */}
+									{/* Special SHIT Rules */}
 									<Grid item xs={12} sx={{ height: "max-content", mb: 2 }}>
-										<Typography variant='h4' component='div' id='SHITRules' className='offset' sx={Styles.sectionHeader}>
-											Special SHIT Rules (“Angel Poo”)
-										</Typography>
-										<Typography variant='body1' component='div' sx={Styles.sectionText}>
-											On all golfing days, the following special rules will apply:
-											<ul style={Styles.unorderedList}>- Mulligan or Reverse Mulligan (1 per round per player, cannot be carried over)</ul>
-											<ul style={Styles.unorderedList}>
-												- Fairway Robbery (1 per round per player) [NB. There are no reverse Fairway Robbery]
-											</ul>
-											<ul style={Styles.unorderedList}>- Big Gimmee (1 per round per player)</ul>
-											<ul style={Styles.unorderedList}>
-												- Joker (1 per round per player) to be called before playing tee shot on any hole for double points on that hole
-											</ul>
-											<ul style={Styles.unorderedList}>- String Theory (Sunday only) - 7 to 1 foot lengths of string drawn at random</ul>
-											<ul style={Styles.unorderedList}>- Big Girl&#39;s Blouse (1 per round per player)</ul>
-										</Typography>
-										<Typography variant='body1' component='div' sx={Styles.sectionText}>
-											Dave Rose (Official Golf Nerd) to hold casting vote any real “Rules of Golf” issues.
-										</Typography>
+										<SpecialSHITRulesSection />
 									</Grid>
 
 									{/* Prizes */}
 									<Grid item xs={12} sx={{ height: "max-content", mb: 0 }}>
-										<Typography variant='h4' component='div' id='prizes' className='offset' sx={Styles.sectionHeader}>
-											Prizes may be available for the following categories:
-										</Typography>
-										<Typography variant='body1' component='div' sx={Styles.sectionText}>
-											<ul style={Styles.unorderedList}>- Overall competition winner (may include trophy)</ul>
-											<ul style={Styles.unorderedList}>- Longest drive (Sunday)</ul>
-											<ul style={Styles.unorderedList}>- Nearest the pin any hole (Sunday)</ul>
-											<ul style={Styles.unorderedList}>- Longest drive (Monday)</ul>
-											<ul style={Styles.unorderedList}>- Nearest the pin any hole (Monday)</ul>
-										</Typography>
+										<PrizesSection />
 									</Grid>
 								</Grid>
 							</PerfectScrollbar>
