7e159bb4e1de03886dfc74552a96590a1599d897
Luke Bangs <bangsluke@gmail.com>
2023-06-27 15:10:44 +0100

    Fixed Fonts meta data warning

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html style={{ scrollBehavior: "smooth" }}>
-			<Head />
+			<Head>
+				{/* Fonts added in _document.tsx due to warning */}
+				<link rel='preconnect' href='https://fonts.gstatic.com' />
+				<link
+					href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&family=Roboto:wght@400;500;700&display=swap'
+					rel='stylesheet'
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
