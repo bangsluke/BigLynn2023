676564d657df6b81b20eff22294a5cd6ca311872
Luke Bangs <bangsluke@gmail.com>
2023-07-04 08:59:17 +0100

    Populated more golf quotes

--- src/data/QuotesData.ts
+++ src/data/QuotesData.ts
@@ -35,6 +35,36 @@ const QuoteArray: Quote[] = [
 		quote: "If you think it's hard to meet new people, try picking up the wrong golf ball.",
 		author: "Jack Lemmon",
 	},
+	{
+		key: uuid(),
+		quote: "Christ this salmon is ****ing salty.",
+		author: "Dave Rose",
+	},
+	{
+		key: uuid(),
+		quote: "The reason the pro tells you to keep your head down is so you can't see him laughing.",
+		author: "Phyllis Diller",
+	},
+	{
+		key: uuid(),
+		quote: "The only sure rule in golf is: He who has the fastest cart never has to play the bad lie.",
+		author: "Mickey Mantle",
+	},
+	{
+		key: uuid(),
+		quote: "I have a tip that can take five strokes off anyone's golf game. It's called an eraser.",
+		author: "Arnold Palmer",
+	},
+	{
+		key: uuid(),
+		quote: "I know I am getting better at golf because I'm hitting fewer spectators.",
+		author: "Gerald Ford",
+	},
+	{
+		key: uuid(),
+		quote: "If you drink, don't drive. Don't even putt.",
+		author: "Dean Martin",
+	},
 ];
 
 export default QuoteArray;
