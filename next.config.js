1598c2b0fa4dbbdda70425db277ca8f4a603b99f
Luke Bangs <bangsluke@gmail.com>
2023-07-14 17:08:04 +0100

    Added comments to next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
+// https://nextjs.org/docs/messages/invalid-next-config
+// https://nextjs.org/docs/pages/api-reference/next-config-js
+const nextConfig = {
+	/* config options here */
 	reactStrictMode: true, // Applies React Strict mode throughout the tool - https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
 	async redirects() {
 		return [
@@ -22,9 +25,12 @@ module.exports = {
 			config.resolve.fallback.fs = false;
 			config.resolve.fallback.tls = false;
 			config.resolve.fallback.net = false;
+			// eslint-disable-next-line
 			config.resolve.fallback.child_process = false;
 		}
 
 		return config;
 	},
 };
+
+module.exports = nextConfig;
